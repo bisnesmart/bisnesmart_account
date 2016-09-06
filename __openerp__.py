1a667f1eddd6cb2c1211effbc21556b225b759e0
Ruben <rcabrera@bisnesmart.com>
2016-09-06 14:46:08 +0200

    Commit inicial

---
+++ __openerp__.py
@@ -0,0 +1,50 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Payment term fixed day",
+    "version": "1.0",
+    "author": "bisnesmart",
+    "category": "Accounting",
+    "website": "http://www.bisnesmart.com",
+    'description' : """
+    Payment term days2 fix.
+    -----------------------
+    When computing a value for a payment day, the next instance of the value
+    in the field days2 is the one selected, instead of the one of the month after.
+
+    Example:
+    --------
+
+    days = 30
+    days2 = 3
+    Current date : 03-01-2016
+
+    30 days after, it will be 02-02-2016. With the default odoo behaviour,
+    the day computed for the payment would be 03-03-2016, but with this module
+    it will be 03-02-2016.
+     
+    """,
+    "depends": [
+        "account",
+    ],
+    "data": [
+    ],
+    'installable': True
+}
---
+++ models/payment.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution
+#    Copyright (C) bisneSmart
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import time
+
+import openerp
+
+from openerp import models
+from openerp.osv import fields, osv, expression
+from openerp.tools.float_utils import float_round as round
+
+class AccountPaymentTerm(models.Model):
+    _inherit = "account.payment.term"
+
+    def compute(self, cr, uid, id, value, date_ref=False, context=None):
+        if not date_ref:
+            date_ref = datetime.now().strftime('%Y-%m-%d')
+        pt = self.browse(cr, uid, id, context=context)
+        amount = value
+        result = []
+        obj_precision = self.pool.get('decimal.precision')
+        prec = obj_precision.precision_get(cr, uid, 'Account')
+        for line in pt.line_ids:
+            if line.value == 'fixed':
+                amt = round(line.value_amount, prec)
+            elif line.value == 'procent':
+                amt = round(value * line.value_amount, prec)
+            elif line.value == 'balance':
+                amt = round(amount, prec)
+            if amt:
+                next_date = (datetime.strptime(date_ref, '%Y-%m-%d') + relativedelta(days=line.days))
+                if line.days2 < 0:
+                    next_first_date = next_date + relativedelta(day=1,months=1) #Getting 1st of next month
+                    next_date = next_first_date + relativedelta(days=line.days2)
+                if line.days2 > 0:
+                    next_date += relativedelta(day=line.days2, months=0)
+                result.append( (next_date.strftime('%Y-%m-%d'), amt) )
+                amount -= amt
+
+        amount = reduce(lambda x,y: x+y[1], result, 0.0)
+        dist = round(value-amount, prec)
+        if dist:
+            result.append( (time.strftime('%Y-%m-%d'), dist) )
+        return result
